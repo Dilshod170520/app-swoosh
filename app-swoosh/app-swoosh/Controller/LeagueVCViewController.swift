f1a299287ba23e1371a1d25eeb978a7df4456158
Dilshod Qulmirzayev <dilshodqulmirzayev21@gmail.com>
2023-09-01 22:16:55 +0500

    we added two new controller

---
+++ app-swoosh/app-swoosh/Controller/LeagueVCViewController.swift
@@ -0,0 +1,20 @@
+//
+//  LeagueVCViewController.swift
+//  app-swoosh
+//
+//  Created by MacBook Pro on 01/09/23.
+//
+
+import UIKit
+
+class LeagueVCViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+
+}
